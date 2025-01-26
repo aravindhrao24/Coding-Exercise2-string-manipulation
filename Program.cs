b8289ee7f1e538d656665504fc71c087e09a48eb
Aravindh Oruganti <138811150+aravindhrao24@users.noreply.github.com>
2025-01-26 21:13:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static void Main(string[] args)
+        {
+            int employeeid = 20;
+            string CompanyName = "UTCLI";
+            Console.WriteLine("string concatenation");
+            Console.WriteLine("hello my employee ID is " + employeeid + " my company name is " + CompanyName);
+            Console.WriteLine("string composite");
+            Console.WriteLine("Hello, my employee ID is {0} and my company name is {1}", employeeid, CompanyName);
+            Console.WriteLine("String interpolation");
+            Console.WriteLine($"Hello my employee ID is {employeeid},my company name is {CompanyName}");
+
+        } 
+
+    }
+}
